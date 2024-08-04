8facae81457fb448cb7213ba0417f3219363cc1a
YUN <116241950+yundogyeong@users.noreply.github.com>
2024-08-04 15:55:21 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -88,6 +88,6 @@ def get_loader():
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
     
     return train_loader, test_loader
